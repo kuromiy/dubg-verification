2e7985598987ad12ce3471844a36867e8052a3d1
ituki <krmy3993@gmail.com>
2020-11-01 22:13:47 +0900

    cors設定追加

--- src/main/java/com/dubg/verification/config/WebConfig.java
+++ src/main/java/com/dubg/verification/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.dubg.verification.config;
 import java.util.Arrays;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -16,5 +17,15 @@ public class WebConfig implements WebMvcConfigurer {
 			.addInterceptor(new AuthenticationHandlerInterceptor())
 			.addPathPatterns("/**")
 			.excludePathPatterns(Arrays.asList("/auth/login"));
+	}
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+				.allowedOrigins("http://localhost:3000")
+				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "PATCH")
+				.allowedHeaders("X-Requested-With", "Origin", "X-Csrftoken", "Content-Type", "Accept")
+				// .allowedHeaders("X-Requested-With, Origin, X-Csrftoken, Content-Type, Accept")
+				.allowCredentials(true);
 	}
 }
